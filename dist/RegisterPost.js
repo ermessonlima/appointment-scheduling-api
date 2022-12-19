6b31a1b42313d7b5d5c3122880ccbe14b68dbf6d
Ermesson Lima <ermessonlima@MacBook-Air-de-Ermesson.local>
2022-12-19 17:52:52 -0300

    feat: add config

--- dist/RegisterPost.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class RegisterPost {
-    execute(name, email, status) {
-        console.log(name, email, status);
-    }
-}
-exports.default = new RegisterPost;
